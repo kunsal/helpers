48e88be23213156733cdc71b7d3c6128e91c28da
Olakunle Salami <kunsal2003@gmail.com>
2020-12-30 14:54:33 +0100

    implemented login feature and tests

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,7 +1,17 @@
 class Api::V1::AuthController < ApplicationController
   def login
-    @user = User.find_by_email(params[:email])
-    puts(@user.inspect)
+    if params[:email].empty? || params[:password].empty?
+      return render json: {message: 'Invalid login credentials'}, status: :unprocessable_entity
+    end
+    user = User.find_by_email(params[:email])
+    if user && BCrypt::Password.new(user.password) == params[:password]
+      payload = {user_id: user.id}
+      token = JWT.encode payload, Rails.application.secrets.secret_key_base, 'HS256'
+      expiry_date = Time.now + 2.days.to_i
+      render json: {token: token, exp: expiry_date.strftime("%m-%d-%Y %H:%M")}
+    else
+      render json: {message: 'Invalid login credentials'}, status: :unauthorized
+    end
   end
 
   private
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -9,7 +9,7 @@ describe 'Users' do
     # Do nothing
   end
 
-  context '/POST user' do
+  context 'Register' do
     it 'creates user in the database and returns 201' do
       expect {
         post '/api/v1/users', params: { user: {
@@ -25,7 +25,7 @@ describe 'Users' do
     end
   end
 
-  context 'User Login' do
+  context 'Login' do
     before(:each) do
       FactoryBot.create(:user, {
         first_name: 'Olakunle',
@@ -38,10 +38,40 @@ describe 'Users' do
         email: 'kunsal@email.com', password: 'password'
       }
     end
+
     it 'should return with error is email is not present' do
       @credentials['email'] = ''
       post '/api/v1/login', params: @credentials
       expect(response).to have_http_status(:unprocessable_entity)
+    end
+
+    it 'should return with error is password is not present' do
+      @credentials['password'] = ''
+      post '/api/v1/login', params: @credentials
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+
+    it 'should return with unauthorized if email is not in database' do
+      @credentials['email'] = 'kay@zmail.co'
+      post '/api/v1/login', params: @credentials
+      expect(response).to have_http_status(:unauthorized)
+    end
+
+    it 'should return with unauthorized if passwords do not match' do
+      @credentials['password'] = 'abcdefg'
+      post '/api/v1/login', params: @credentials
+      expect(response).to have_http_status(:unauthorized)
+    end
+
+    it 'should log user in with valid credential' do
+      post '/api/v1/login', params: @credentials
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'should return token when login is successful' do
+      post '/api/v1/login', params: @credentials
+      puts response.body.inspect
+      expect(JSON.parse(response.body).keys).to match_array(%w[token exp])
     end
   end
 end
