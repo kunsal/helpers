55bb68279d4d2243683dd606f3760ce69577df94
Olakunle Salami <kunsal2003@gmail.com>
2021-01-05 10:44:34 +0100

    added exp and iat claims to jwt token

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -7,7 +7,7 @@ class Api::V1::AuthController < ApplicationController
 
     if user &. authenticate(params[:password])
       expiry_date = (Time.now + 2.days).to_i
-      payload = {user_id: user.id, exp: expiry_date}
+      payload = {user_id: user.id, exp: expiry_date, iat: Time.now.to_i}
       token = JWT.encode payload, Rails.application.secrets.secret_key_base, 'HS256'
       render json: {token: token}, status: :ok
     else
