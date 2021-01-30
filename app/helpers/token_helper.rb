70971b875d2bac89a8cb61e27b745e5bdf13ad14
Olakunle Salami <kunsal2003@gmail.com>
2021-01-30 23:09:31 +0100

    removed extra secrets from secret_key_base

--- app/helpers/token_helper.rb
+++ app/helpers/token_helper.rb
@@ -2,10 +2,10 @@ module TokenHelper
   def generate(user)
     expiry_date = (Time.now + 2.days).to_i
     payload = {user_id: user.id, exp: expiry_date, iat: Time.now.to_i}
-    JWT.encode payload, Rails.application.secrets.secret_key_base, 'HS256'
+    JWT.encode payload, Rails.application.secret_key_base, 'HS256'
   end
 
   def decode(token)
-    JWT.decode(token, Rails.application.secrets.secret_key_base, true, algorithm: 'HS256')
+    JWT.decode(token, Rails.application.secret_key_base, true, algorithm: 'HS256')
   end
 end
