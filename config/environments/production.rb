70fd8965474497e2ffec345cbd9173938c8ca909
Olakunle Salami <kunsal2003@gmail.com>
2021-02-01 09:00:54 +0100

    set ws configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,6 +110,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.web_socket_server_url = "wss://kunsal-helpers.herokuapp.com/cable"
-  config.action_cable.allowed_request_origins = ['https://kunsal-helpers.herokuapp.com', 'http://kunsal-helpers.herokuapp.com']
+  config.action_cable.url = [/ws:\/\/*/, /wss:\/\/*/]
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
+  # config.action_cable.allowed_request_origins = ['https://kunsal-helpers.herokuapp.com', 'http://kunsal-helpers.herokuapp.com']
 end
