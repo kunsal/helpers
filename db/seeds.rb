d57ad67033bccafb2d92e95a157436bab176953f
Olakunle Salami <kunsal2003@gmail.com>
2021-02-02 01:32:43 +0100

    added help update seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-# Category.destroy_all
+Category.destroy_all
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -8,8 +8,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # Category.delete_all
-# Category.create([{name: 'One Time Need', color: 'blue'}, {name: 'Material Need', color: 'green'}])
+category = Category.create([{name: 'One Time Need', color: 'blue'}, {name: 'Material Need', color: 'green'}])
 help = Help.find(1)
-help.category_id = 4
+help.category_id = category.first.id
 help.save
 
